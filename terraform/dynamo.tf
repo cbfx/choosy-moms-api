121718891d06d31270a3a102d74bac788cd43d82
Chris Burrell <chrisburrell@gmail.com>
2018-11-27 14:59:06 -0600

    feat(terraform): reindex dynamo table

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -21,7 +21,7 @@ resource "aws_dynamodb_table" "saved" {
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "userId"
-  range_key      = "id"
+  range_key      = "gifId"
 
   attribute {
     name = "userId"
@@ -29,7 +29,7 @@ resource "aws_dynamodb_table" "saved" {
   }
 
   attribute {
-    name = "id"
+    name = "gifId"
     type = "S"
   }
 }
