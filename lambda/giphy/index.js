e5b10c338f1cbc37885e3b8050105bd878a1f4ed
Chris Burrell <chrisburrell@gmail.com>
2018-11-27 03:04:20 -0600

    feat(lambda.giphy): add giphy proxy

--- lambda/giphy/index.js
+++ lambda/giphy/index.js
@@ -1,13 +1,38 @@
-exports.handler = function(event, context, callback) {
-  'use strict';
+'use strict';
 
-  const response = {
-    isBase64Encoded: false,
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({"lambda": "giphy"}),
+const querystring = require('querystring');
+const fetch = require('node-fetch');
+const GIPHY = 'https://api.giphy.com';
+
+exports.handler = function(event, context, callback) {
+  let fetchParams = {
+    method: event.httpMethod
   };
-  callback(null, response);
+  if (event.body) {
+    fetchParams.body = JSON.stringify(event.body);
+  }
+  const queryParams = querystring.stringify(
+    Object.assign(event.queryStringParameters || {}, {
+      api_key: process.env.GIPHY_API_KEY
+    })
+  );
+
+  const giphyUrl = `${GIPHY}/${event.pathParameters.proxy}?${queryParams}`;
+  return fetch(giphyUrl, fetchParams)
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      callback(null, {
+        isBase64Encoded: false,
+        statusCode: json.meta.status,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(json)
+      });
+    })
+    .catch((err) => {
+      return callback(err);
+    });
 };
